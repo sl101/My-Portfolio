ac02e1d26f551f05b102778fd2b648ad433d010f
Slava <v.zhevaga@gmail.com>
2024-10-30 20:11:46 +0100

    commited lint error

--- app/components/templates/Navigation.tsx
+++ app/components/templates/Navigation.tsx
@@ -1,15 +1,13 @@
 "use client";
 import Link from "next/link";
-//import { usePathname } from "next/navigation";
 
 export const Navigation = () => {
-	//const pathname = usePathname();
 
 	const linksList = [
 		{ id: 1, path: "#about", value: "About" },
 		{ id: 2, path: "#contacts", value: "Contacts" },
 	];
-	//console.log(pathname);
+
 	return (
 		<nav>
 			<ul className="flex-between-center gap-4">
