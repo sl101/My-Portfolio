0a3c13f2bffecdd70d3af2e7efb78f83e6f6093a
Slava <v.zhevaga@gmail.com>
2024-10-30 20:38:47 +0100

    lint for ContactForm

--- app/components/templates/ContactForm.tsx
+++ app/components/templates/ContactForm.tsx
@@ -3,10 +3,10 @@ import { sendForm } from "@/app/apiRequests/sendEmailForm";
 export const ContactForm  = () =>{
 	async function sendEmailForm(form: FormData) {
 		"use server";
-		const name: any = form.get("name")?.toString();
-		const phone: any = form.get("phone")?.toString();
-		const email: any = form.get("email")?.toString();
-		const message: any = form.get("message")?.toString();
+		const name: string = form.get("name")?.toString() || "";
+		const phone: string = form.get("phone")?.toString() || "";
+		const email: string = form.get("email")?.toString() || "";
+		const message: string = form.get("message")?.toString() || "";
 
 		const feedback = {
 			name,
