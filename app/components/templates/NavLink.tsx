3abfd302bcc26948abc30d9b8008efc7d739224e
Slava <v.zhevaga@gmail.com>
2024-11-08 23:35:43 +0100

    add hover navlink

--- app/components/templates/NavLink.tsx
+++ app/components/templates/NavLink.tsx
@@ -17,7 +17,7 @@ export const NavLink = ({link}:Props) => {
 	return (
 		<li key={link.id}>
 						<Link
-							className=""
+							className="pb-1 outline-none focus:text-accent-orange border-b-2  hover:text-accent-orange hover:border-accent-orange transition-color duration-500 " 
 							href={link.path}
 							target={link.target}
 						>
