619681b09a29bc142db13fcfc5a31a1da4a3c53a
Slava <v.zhevaga@gmail.com>
2024-11-12 00:28:12 +0100

    fix burger close with logo

--- app/components/elements/Logo.tsx
+++ app/components/elements/Logo.tsx
@@ -1,22 +1,14 @@
 "use client"
-import { closeMenu, openMenu } from "@/app/context/burger";
-import { useBurger } from "@/app/hooks/useBurger";
+import { closeMenu } from "@/app/context/burger";
 import Link from "next/link";
 
 export const Logo = () => {
-	const isActive  = useBurger();
-	const handleSwitchBurger = ()=>{
-		if(isActive){
-			closeMenu()
-		} else {
-			openMenu()
-		}
-	}
+
 	return (
 		<div className="font-['Roboto-medium'] relative z-50">
 			<Link className="flex-col text-lg/5 uppercase group focus:outline-none" 
 			href="#hero"
-			onClick={handleSwitchBurger}
+			onClick={()=>closeMenu()}
 			>
 				<p className="text-accent-orange transition-colors duration-500  group-hover:text-white">Viacheslav</p>
 				<p className="transition-colors duration-500 group-hover:text-accent-orange">Zhevaha</p>
